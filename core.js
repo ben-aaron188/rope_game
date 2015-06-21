513eee97a0aef2db7b675252f444a54ac3ededa8
ben-aaron188 <bennettkleinberg@BAR-MacBook-Air.local>
2015-06-21 18:02:30 +0100

    new controls

--- core.js
+++ core.js
@@ -34,3 +34,17 @@ function css_center(object, relative_object) {
     "margin-top": -($(relative_object).height() / 2)
   })
 }
+
+$.fn.getdegree = function() {
+    var el = $(this),
+        tr = el.css("-webkit-transform") || el.css("-moz-transform") || el.css("-ms-transform") || el.css("-o-transform") || '',
+        info = {rad: 0, deg: 0};
+    if (tr = tr.match('matrix\\((.*)\\)')) {
+        tr = tr[1].split(',');
+        if(typeof tr[0] != 'undefined' && typeof tr[1] != 'undefined') {
+            info.rad = Math.atan2(tr[1], tr[0]);
+            info.deg = parseFloat((info.rad * 180 / Math.PI).toFixed(1));
+        }
+    }
+    return info;
+};
--- func.js
+++ func.js
@@ -1,63 +1,101 @@
-var c, ctx,
-  Angle = 0,
+var Angle_pos = Angle_neg = 0,
+  angle = 10,
   count = 0,
   movementID;
 
 $(document)
   .ready(function() {
     css_center($("#player"))
-      //init_player()
   })
-  .keydown(function() {
-    hori_move("neg", 20)
+  .keydown(function(e) {
+    console.log(Angle_pos)
+    console.log(Angle_neg)
+    var code = e.keyCode || e.which;
+    switch (code) {
+      case 39:
+        correct_pos()
+        break;
+      case 37:
+        correct_neg()
+        break;
+    }
   })
   .keyup(function() {
     count = 0
     cancelAnimationFrame(movementID);
   })
 
-function init_player() {
-  c = $("#canvas")[0];
-  ctx = c.getContext("2d");
-  ctx.moveTo(225, 500);
-  ctx.lineTo(225, 200);
-  ctx.lineWidth = 15
-  ctx.stroke();
+
+function get_rotation_degree(){
+  $("#player").getdegree().deg
 }
 
-
-function hori_move(xdir, angle) {
+function correct_pos() {
   movementID = requestAnimationFrame(function() {
-    hori_move(xdir, angle)
+    correct_pos()
   });
   count++
   if (count < 2) {
-    Angle += angle
-    switch (xdir) {
-      case "pos":
-        $("#player").css({
-          "-webkit-transform": "rotate(" + Angle + "deg)",
-          "-moz-transform": "rotate(" + Angle + "deg)",
-          "-ms-transform": "rotate(" + Angle + "deg)",
-          "-o-transform": "rotate(" + Angle + "deg)",
-          "transform": "rotate(" + Angle + "deg)",
-          "zoom": 1
-        })
-        break;
-      case "neg":
-        $("#player").css({
-          "-webkit-transform": "rotate(-" + Angle + "deg)",
-          "-moz-transform": "rotate(-" + Angle + "deg)",
-          "-ms-transform": "rotate(-" + Angle + "deg)",
-          "-o-transform": "rotate(-" + Angle + "deg)",
-          "transform": "rotate(-" + Angle + "deg)",
-          "zoom": 1
-        })
-        break;
-    }
+    Angle_pos += angle;
+    $("#player").css({
+      "-webkit-transform": "rotate(" + Angle_pos + "deg)",
+      "-moz-transform": "rotate(" + Angle_pos + "deg)",
+      "-ms-transform": "rotate(" + Angle_pos + "deg)",
+      "-o-transform": "rotate(" + Angle_pos + "deg)",
+      "transform": "rotate(" + Angle_pos + "deg)",
+      "zoom": 1
+    })
   }
 }
 
-function corrector() {
+function correct_neg() {
+  movementID = requestAnimationFrame(function() {
+    correct_neg()
+  });
+  count++
+  if (count < 2) {
+    Angle_neg += angle;
+    $("#player").css({
+      "-webkit-transform": "rotate(-" + Angle_neg + "deg)",
+      "-moz-transform": "rotate(-" + Angle_neg + "deg)",
+      "-ms-transform": "rotate(-" + Angle_neg + "deg)",
+      "-o-transform": "rotate(-" + Angle_neg + "deg)",
+      "transform": "rotate(-" + Angle_neg + "deg)",
+      "zoom": 1
+    })
+  }
+}
 
+
+function corrector() {
+  $(document)
+    .keydown(function(e) {
+      var code = e.keyCode || e.which;
+      movementID = requestAnimationFrame(function() {
+        corrector()
+      });
+      count++
+      if (count < 2) {
+        Angle += angle
+        if (code == 39) {
+          $("#player").css({
+            "-webkit-transform": "rotate(" + Angle + "deg)",
+            "-moz-transform": "rotate(" + Angle + "deg)",
+            "-ms-transform": "rotate(" + Angle + "deg)",
+            "-o-transform": "rotate(" + Angle + "deg)",
+            "transform": "rotate(" + Angle + "deg)",
+            "zoom": 1
+          })
+        } else if (code == 37) {
+          $("#player").css({
+            "-webkit-transform": "rotate(-" + Angle + "deg)",
+            "-moz-transform": "rotate(-" + Angle + "deg)",
+            "-ms-transform": "rotate(-" + Angle + "deg)",
+            "-o-transform": "rotate(-" + Angle + "deg)",
+            "transform": "rotate(-" + Angle + "deg)",
+            "zoom": 1
+          })
+        }
+      }
+    })
 }
